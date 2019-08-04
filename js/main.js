70f34166f4589f0e75c7e23910e80fcc848dd16d
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-04 21:12:01 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,21 +4,10 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '小吉'];
-    const n = Math.floor(Math.random() * 3);
+    const results = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * results.length);
     btn.textContent = results[n];
-    // btn.textContent = n;
-    // switch(n){
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break;
-    //   case 1:
-    //     btn.textContent = '中吉';
-    //     break;
-    //   case 2:
-    //     btn.textContent = '小吉';
-    //     break;
-    // }
+
   });
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
