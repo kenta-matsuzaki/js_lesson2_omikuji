820ab2adee4b2e013f326178c1d3c698cd05668f
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-04 21:10:50 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,21 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const results = ['大吉', '中吉', '小吉'];
+    const n = Math.floor(Math.random() * 3);
+    btn.textContent = results[n];
+    // btn.textContent = n;
+    // switch(n){
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '小吉';
+    //     break;
+    // }
   });
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
